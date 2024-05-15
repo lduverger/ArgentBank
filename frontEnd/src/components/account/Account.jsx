bc0391d686c7f9bf6148ffcd09b9605effb77775
lduverger <duverger.loic@live.fr>
2024-05-15 09:57:59 +0200

    forgot to replace class by className

--- frontEnd/src/components/account/Account.jsx
+++ frontEnd/src/components/account/Account.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Account = ({ title, amount, aDesc }) => {
   return (
-    <section class="account">
-      <div class="account-content-wrapper">
-        <h3 class="account-title">{title}</h3>
-        <p class="account-amount">{amount}</p>
-        <p class="account-amount-description">{aDesc}</p>
+    <section className="account">
+      <div className="account-content-wrapper">
+        <h3 className="account-title">{title}</h3>
+        <p className="account-amount">{amount}</p>
+        <p className="account-amount-description">{aDesc}</p>
       </div>
-      <div class="account-content-wrapper cta">
-        <button class="transaction-button">View transactions</button>
+      <div className="account-content-wrapper cta">
+        <button className="transaction-button">View transactions</button>
       </div>
     </section>
   );
--- frontEnd/src/pages/signIn/SignIn.jsx
+++ frontEnd/src/pages/signIn/SignIn.jsx
@@ -5,25 +5,25 @@ import { NavLink } from 'react-router-dom';
 
 const SignIn = () => {
     return (
-        <main class="main bg-dark">
-        <section class="sign-in-content">
+        <main className="main bg-dark">
+        <section className="sign-in-content">
         <FontAwesomeIcon icon={faCircleUser} className='sign-in-icon'/>
-          <i class="fa fa-user-circle sign-in-icon"></i>
+          <i className="fa fa-user-circle sign-in-icon"></i>
           <h1>Sign In</h1>
           <form>
-            <div class="input-wrapper">
+            <div className="input-wrapper">
               <label for="username">Username</label>
               <input type="text" id="username" />
             </div>
-            <div class="input-wrapper">
+            <div className="input-wrapper">
               <label for="password">Password</label>
               <input type="password" id="password" />
             </div>
-            <div class="input-remember">
+            <div className="input-remember">
               <input type="checkbox" id="remember-me" />
               <label for="remember-me">Remember me</label>
             </div>
-            <NavLink to="/user" class="sign-in-button">Sign In</NavLink>
+            <NavLink to="/user" className="sign-in-button">Sign In</NavLink>
             {
                 /*
                 <!-- SHOULD BE THE BUTTON BELOW -->
